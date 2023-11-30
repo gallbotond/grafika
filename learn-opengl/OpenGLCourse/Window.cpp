349479a1881400d0f94c5e98c0a0daa225ab49e4
Gáll Botond <gallbotondofficial@gmail.com>
2023-11-30 02:53:59 +0200

    abstract into classes

--- learn-opengl/OpenGLCourse/Window.cpp
+++ learn-opengl/OpenGLCourse/Window.cpp
@@ -1,25 +1,79 @@
-#include <stdio.h>
-#include <string.h>
-#include <cmath>
-#include <vector>
-
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 #include "Window.h"
-#include "Mesh.h"
-#include "Shader.h"
 
-const float toRadians = 3.14159265f / 180.0f;
+Window::Window()
+{
+	width = 800;
+	height = 600;
 
-Window mainWindow;
-std::vector<Mesh*> meshList;
-std::vector<Shader> shaderList;
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+}
 
-// Vertex Shader
-static const shar* vShader = "shaders/shader.vert";
+Window::Window(GLint windowWidth, GLint windowHeight)
+{
+	width = windowWidth;
+	height = windowHeight;
 
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+}
+
+int Window::Initialize()
+{
+	// Initialize GLFW
+	if (!glfwInit())
+	{
+		printf("GLFW initialization failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup GLFW window properties
+	// OpenGL version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Core Profile = No Backwards Compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Allow Forward Compatibility
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	mainWindow = glfwCreateWindow(width, height, "Test Window", NULL, NULL);
+	if (!mainWindow)
+	{
+		printf("GLFW window creation failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get Buffer Size information
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	// Set context for GLEW to use
+	glfwMakeContextCurrent(mainWindow);
+
+	// Handle Key + Mouse Input
+	createCallbacks();
+	glfwSetInputMode(mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("GLEW initialization failed!");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	// Setup Viewport size
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	glfwSetWindowUserPointer(mainWindow, this);
+}
