e941302b17201cabaf692832f2ab654e6374c800
Gáll Botond <gallbotondofficial@gmail.com>
2023-12-01 02:48:16 +0200

    added imgui and binded to translation sliders

--- learn-opengl/OpenGL_Cherno/src/Application.cpp
+++ learn-opengl/OpenGL_Cherno/src/Application.cpp
@@ -12,8 +12,15 @@
 #include "VertexBufferLayout.h"
 #include "Indexbuffer.h"
 #include "VertexArray.h"
-#include "Shader.h"
+#include "Shader.h" 
+#include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 int main(void)
 {
@@ -48,10 +55,10 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	float positions[] = {
-		-0.5f, -0.5f,   // 0
-		 0.5f, -0.5f,   // 1
-		 0.5f,  0.5f,   // 2
-		-0.5f,  0.5f,   // 3
+		-0.5f, -0.5f, 0.0f, 0.0f,  // 0
+		 0.5f, -0.5f, 1.0f, 0.0f,  // 1
+		 0.5f,  0.5f, 1.0f, 1.0f,  // 2
+		-0.5f,  0.5f, 0.0f, 1.0f,  // 3
 	};
 
 	unsigned int indices[] = {
@@ -59,22 +66,29 @@ int main(void)
 		2, 3, 0
 	};
 
-	unsigned int vao;
-	GLCall(glGenVertexArrays(1, &vao));
-	GLCall(glBindVertexArray(vao));
+	GLCall(glEnable(GL_BLEND));
+	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
 	VertexArray va;
-	VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+	VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
 	VertexBufferLayout layout;
+	layout.Push<float>(2);
 	layout.Push<float>(2);
 	va.AddBuffer(vb, layout);
 
 	IndexBuffer ib(indices, 6);
 
+	glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f); // Orthographic projection matrix
+	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-1, 0, 0)); // View matrix
+
 	Shader shader("res/shaders/Basic.shader");
 	shader.Bind();
 	shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+	Texture texture("res/textures/building.png");
+	texture.Bind();
+	shader.SetUniform1i("u_Texture", 0);
 
 	va.Unbind();
 	vb.Unbind();
@@ -83,16 +97,42 @@ int main(void)
 
 	Renderer renderer;
 
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
+	ImGui::StyleColorsDark();
+
+	glm::vec3 translation(1, 1, 0);
+
 	float r = 0.0f;
 	float increment = 0.05f;
+
+	bool show_window = true;
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		renderer.Clear();
 
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), translation); // Model 
+		glm::mat4 mvp = proj * view * model;
+
+		//ImGui::ShowDemoWindow(); // test if imgui works
+		{
+			static float f = 0.0f;
+
+			ImGui::SliderFloat("Translation x", &translation.x, -1.0f, 1.0f);
+			ImGui::SliderFloat("Translation y", &translation.y, -1.0f, 1.0f);
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		}
+
 		shader.Bind();
 		shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
 		renderer.Draw(va, ib, shader);
 
@@ -104,6 +144,9 @@ int main(void)
 			increment = 0.05f;
 		r += increment;
 
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 		/* Swap front and back buffers */
 		GLCall(glfwSwapBuffers(window));
 
@@ -111,6 +154,12 @@ int main(void)
 		GLCall(glfwPollEvents());
 	}
 
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
--- learn-opengl/OpenGL_Cherno/src/Shader.h
+++ learn-opengl/OpenGL_Cherno/src/Shader.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "glm/glm.hpp"
+
 struct ShaderProgramSource
 {
 	std::string VertexSource;
@@ -13,7 +15,7 @@ class Shader {
 private:
 	std::string m_FilePath;
 	unsigned int m_RendererID;
-	std::unordered_map<std::string, unsigned int> m_UniformLocationCache;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
 public:
 	Shader(const std::string& filepath);
 	~Shader();
@@ -23,11 +25,13 @@ public:
 
 	// set uniforms
 	void SetUniform1i(const std::string& name, int value);
-	void SetUniform4f(const std::string& name, float v0, float v1, float f2, float f3);
+	void SetUniform1f(const std::string& name, float value);
+	void SetUniform4f(const std::string& name, float v0, float v1, float f2, float f3); 
+	void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 private:
 	bool CompileShader();
 	ShaderProgramSource ParseShader(const std::string& filepath);
-	unsigned int GetUniformLocation(const std::string& name);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
+	int GetUniformLocation(const std::string& name);
 };
